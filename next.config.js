45bb9e4b519b83bda8b0e6d8323ec1be8f63d302
PaulSamPS <19071990Pqw>
2022-09-25 20:49:07 +0500

    eslint, prettier, stylelint, next config, custom d.ts

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
 
-module.exports = nextConfig
+const nextConfig = {
+  swcMinify: true,
+  images: {
+    domains: ['localhost:5000']
+  },
+  reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack']
+    });
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
