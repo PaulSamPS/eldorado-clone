b345f95f6289d3589f287c87d623b2d41aae1dee
Pavel Samoylenko <psamoylenko2@gmail.com>
2022-11-05 07:18:22 +0300

    .

--- components/Basket/OrderConfirmation/OrderConfirmation.tsx
+++ components/Basket/OrderConfirmation/OrderConfirmation.tsx
@@ -1,22 +1,45 @@
 import React from 'react';
+import NumbrerFormat from 'react-number-format';
 import { Total } from '@/components/ReusableComponents';
 import { H } from '@/components/Ui';
 import styles from './OrderConfirmation.module.scss';
 
-export const OrderConfirmation = () => (
-  <div className={styles.wrapper}>
-    <H tag='h2'>Контактное лицо</H>
-    <span>все поля формы обязательны для заполнения</span>
-    <div className={styles.form}>
-      <div className={styles.input}>
-        <input name='email' type='email' />
-        <label htmlFor='email'>E-mail</label>
+type InputValueState = {
+  formattedValue: string;
+  value: string;
+};
+
+export const OrderConfirmation = () => {
+  const [values, setValues] = React.useState<InputValueState>({} as InputValueState);
+  return (
+    <div className={styles.wrapper}>
+      <H tag='h2'>Контактное лицо</H>
+      <span>все поля формы обязательны для заполнения</span>
+      <div className={styles.form}>
+        <div className={styles.input}>
+          <NumbrerFormat
+            className='field'
+            format='+# (###) ###-##-##'
+            mask='_'
+            placeholder='+7 (123) 456-78-90'
+            value={values.value}
+            onValueChange={({ formattedValue, value }) => setValues({ formattedValue, value })}
+          />
+        </div>
+        <div className={styles.input}>
+          <input name='e-mail' type='email' />
+          <label htmlFor='e-mail'>E-mail</label>
+        </div>
+        <div className={styles.input}>
+          <input name='first name' type='text' />
+          <label htmlFor='first name'>Имя</label>
+        </div>
+        <div className={styles.input}>
+          <input name='last name' type='text' />
+          <label htmlFor='last name'>Фамилия</label>
+        </div>
       </div>
-      <div className={styles.input}>
-        <input name='name' type='text' />
-        <label htmlFor='name'>Имя</label>
-      </div>
+      <Total />
     </div>
-    <Total />
-  </div>
-);
+  );
+};
